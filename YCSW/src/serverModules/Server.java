b641ed03547bf993cad2ffcba04a32c1b0bd6671
Rohan Limaye <50995906+rylp@users.noreply.github.com>
2020-11-07 19:22:51 +0530

    Minor Changes

--- YCSW/src/serverModules/Server.java
+++ YCSW/src/serverModules/Server.java
@@ -1,5 +1,7 @@
 package serverModules;
 
+import java.net.Inet4Address;
+
 public class Server {
 	
 	public static void main(String[] args) throws Exception
@@ -7,6 +9,12 @@ public class Server {
 		System.out.println("Server Started");
 		
 		System.out.println("Waiting for connection");
+		
+		System.out.println("Server IP:"+Inet4Address.getLocalHost());
+		
+		String ip=String.valueOf(Inet4Address.getLocalHost()).split("/")[1];
+		
+		System.out.println(ip);
 		
 		Connection con=new Connection();
 		Constants.threadPool.submit(con);
