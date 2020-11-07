1a3ff22db64c9c9485d275914a1556c65c8f8906
Rohan Limaye <50995906+rylp@users.noreply.github.com>
2020-11-07 19:23:34 +0530

    Minor changes

--- YCSW-ClientSide/src/client/ClientConnection.java
+++ YCSW-ClientSide/src/client/ClientConnection.java
@@ -10,7 +10,11 @@ public class ClientConnection
 		try
 		{
 			//Should come from GUI.
-			String ip="localhost";
+//			String ip="localhost";
+			
+			String ip="192.168.0.28";
+
+//			String ip=String.valueOf(Inet4Address.getLocalHost()).split("/")[1];
 			int port=9997;
 			
 			System.out.println("Connection Class");
@@ -22,10 +26,12 @@ public class ClientConnection
 			Constants.clientIp=socket.getInetAddress().getHostAddress();
 			Constants.clientPort=socket.getLocalPort();
 			
+			System.out.println("Client IP:"+Constants.clientIp);
+			System.out.println("Client Port:"+Constants.clientPort);
+			
 			System.out.println("Connection Established");
 			
 			ClientCommunication clientComm=new ClientCommunication(socket);
-			
 		}
 		catch(Exception e)
 		{
